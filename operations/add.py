3b5180e2e2ea383270b6e10092d3fae9e5f20e82
SachinShaju <75465688+SachinShaju@users.noreply.github.com>
2025-08-11 18:05:44 +0530

    Update add.py

--- operations/add.py
+++ operations/add.py
@@ -1,9 +1,11 @@
-def add(x, y): 
+"""
+This module provides the `add` function for adding two numbers.
+"""
+def add(x, y):
     """
     Args:
         x: The 1st number for addition.
         y: The 2nd number for addition.
-    
     Returns:
         The sum.
     """
